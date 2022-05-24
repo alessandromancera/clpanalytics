025cf7639cacee42a5a421db3dca8245c857d880
Alessandro Lima Mancera <alessandro.mancera@hotmail.com>
2022-05-24 21:23:08 +0000

    Add url api

--- src/pages/Relatorios.vue
+++ src/pages/Relatorios.vue
@@ -3,15 +3,42 @@
       <section class="container">
         <h1>Relatórios</h1>
 
+        <div id="example-1">
+          <button v-on:click="Carregar('esteira')">Esteira</button>
+          <button v-on:click="Carregar('forno')">Forno</button>
+          <button v-on:click="Carregar('flowpack')">Flowpack</button>
+        </div>
+
       </section>
 
     </main>
 </template>
 
 <script>
+import api from '@/services/api.js'
 
 export default {
-  name: 'cReport'
+  name: 'cReport',
+  data: () => ({
+  }),
+  mounted () {
+
+  },
+  methods: {
+    Carregar (typeclick) {
+      const url = `/${typeclick}`
+      // alert(url)
+      api.get(url)
+        .then(response => {
+          console.log(response)
+        })
+        .catch(error => {
+          console.log('Error')
+          console.log(error)
+        })
+        .finally(() => console.log('Finalizado'))
+    }
+  }
 }
 </script>
 
--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'https://'
+  baseURL: 'https://dry-plateau-13546.herokuapp.com/'
 })
 
 export default api
