ed50614b233c1fab71806d3302395e01b0be4219
Alessandro Lima Mancera <alessandro.mancera@hotmail.com>
2022-05-26 11:28:57 +0000

    Conect api

--- src/pages/Relatorios.vue
+++ src/pages/Relatorios.vue
@@ -1,50 +1,86 @@
 <template>
-    <main>
-      <section class="container">
-        <h1>Relatórios</h1>
+  <main>
+    <section class="container">
+      <h1>Relatórios</h1>
 
-        <div id="example-1">
-          <button v-on:click="Carregar('esteira')">Esteira</button>
-          <button v-on:click="Carregar('forno')">Forno</button>
-          <button v-on:click="Carregar('flowpack')">Flowpack</button>
-        </div>
+      <div id="example-1">
+        <button v-on:click="Carregar('esteira')">Esteira</button>
+        <button v-on:click="Carregar('forno')">Forno</button>
+        <button v-on:click="Carregar('flowpack')">Flowpack</button>
+      </div>
 
-      </section>
+      <div id="esteira">
+        <v-client-table
+          :data="usersCollection"
+          :options="tableOptions"
+          :columns="tableColumns">
+        </v-client-table>
 
-    </main>
+        <vuetable ref="vuetable"
+          :api-mode="false"
+          :data="data"
+          :fields="tableColumns"
+          data-path="rows"
+          pagination-path=""
+        ></vuetable>
+      </div>
+
+    </section>
+  </main>
 </template>
 
 <script>
 import api from '@/services/api.js'
+import Vuetable from 'vuetable-2'
 
 export default {
   name: 'cReport',
-  data: () => ({
-  }),
-  mounted () {
-
+  components: {
+    Vuetable
   },
+  data () {
+    return {
+      tableColumns: ['id', 'velocidade_rolo', 'velocidade_esteira', 'timestamp'],
+      usersCollection: [],
+      data: [],
+      tableOptions: {
+        headings: {
+          id: 'ID',
+          velocidade_rolo: 'Velocidade Rolo',
+          velocidade_esteira: 'Velocidade Esteira',
+          timestamp: 'Data Hora'
+        }
+        // sortable: ['id', 'timestamp'],
+        // filterable: ['id', 'timestamp']
+      }
+    }
+  },
+  mounted () {},
   methods: {
     Carregar (typeclick) {
       const url = `/${typeclick}`
-      // alert(url)
-      api.get(url)
-        .then(response => {
-          console.log(response)
-        })
-        .catch(error => {
-          console.log('Error')
-          console.log(error)
-        })
-        .finally(() => console.log('Finalizado'))
+      if (url !== '') {
+        api.get(url)
+          .then((response) => {
+            this.show_esteira = true
+            this.data = JSON.stringify(response.data)
+            this.usersCollection = JSON.stringify(response.data)
+            console.log('usersCollection')
+            console.log(this.usersCollection)
+          })
+          .catch((error) => {
+            console.log('Error')
+            console.log(error)
+          })
+          .finally(() => console.log('Finalizado'))
+      }
     }
   }
 }
 </script>
 
 <style scoped>
-
 main {
-      align-items: center;
-    }
+  align-items: center;
+}
 </style>
